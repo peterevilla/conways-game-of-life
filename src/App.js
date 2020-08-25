a12c8584f83940a785105422685286c6368afb7a
IT2 <it2@AWT.local>
2020-08-25 14:27:36 -0500

    addig features

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './App.css';
 import Grid from './components/Grid'
 import ControlBoard from './components/ControlBoard'
+import Rules from './components/Rules'
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div className='conway'>
       <h1>Conway's Game of Life</h1>
+      <div className="App">
+      
       <div className="dash">
         <Grid/>
+        <ControlBoard/>
       </div>
-      <ControlBoard/>
+      <div className='rules'>
+      <Rules/>
+      </div>
     </div>
+    </div>
+    
   );
 
 }
